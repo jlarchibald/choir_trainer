81d08e51eb4086da3b657416fd029449239a27b1
James Lawrence Archibald II <jlarchibald@gmail.com>
2015-02-24 08:49:13 -0600

    chat messages sent to console.

--- index.js
+++ index.js
@@ -9,7 +9,15 @@ console.log('we served one webpage');
 
 io.on('connection', function(socket){
 console.log('a user connected');
+socket.on('disconnect',function(){
+console.log('user disconnected');
 });
+socket.on('chat message',function(msg){
+    console.log('message: ' + msg);
+});
+});
+
+
 
 http.listen(3000,function(){
 console.log('listening on *:3000');
